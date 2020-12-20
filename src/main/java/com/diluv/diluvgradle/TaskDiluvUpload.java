0938ce895d899b088ad5643f32eb924b568f514f
Tyler Hancock <darklime@live.ca>
2020-12-20 09:42:47 -0700

    Started reorganization of the upload task.

--- src/main/java/com/diluv/diluvgradle/TaskDiluvUpload.java
+++ src/main/java/com/diluv/diluvgradle/TaskDiluvUpload.java
@@ -5,13 +5,10 @@ import java.io.IOException;
 import java.lang.reflect.Method;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import javax.annotation.Nullable;
@@ -46,29 +43,16 @@ import com.google.gson.GsonBuilder;
 public class TaskDiluvUpload extends DefaultTask {
     
     /**
-     * Logger for internal use only.
+     * An internal logger instance used to output status and debug information about the plugin
+     * and it's usage.
      */
-    private static final Logger LOGGER = Logging.getLogger("DiluvGradle");
+    private final Logger log;
     
     /**
-     * Constant gson instance used for deserializing the API responses when files are uploaded.
+     * A project specific instance of Gson. Used to serialize JSON objects for use with Diluv's
+     * REST API.
      */
-    private static final Gson GSON = new GsonBuilder().create();
-    
-    /**
-     * A regex pattern for matching semantic versioning version numbers. This was taken from
-     * https://semver.org/.
-     */
-    private static final Pattern SEM_VER = Pattern.compile("^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+([0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?$");
-    
-    private static final String RELATION_REQUIRED = "required";
-    private static final String RELATION_OPTIONAL = "optional";
-    private static final String RELATION_INCOMPATIBLE = "incompatible";
-    
-    /**
-     * A list of recognized project relationship types.
-     */
-    private static final List<String> PROJECT_RELATION_TYPES = Arrays.asList(RELATION_REQUIRED, RELATION_OPTIONAL, RELATION_INCOMPATIBLE);
+    private final Gson gson;
     
     /**
      * The URL used for communicating with Diluv. This should not be changed unless you know
@@ -147,6 +131,10 @@ public class TaskDiluvUpload extends DefaultTask {
     
     public TaskDiluvUpload() {
         
+        this.log = Logging.getLogger("DiluvGradle");
+        
+        this.gson = new GsonBuilder().create();
+        
         // If the build task is present make sure this task is ran after it. This is required
         // for some environments such as those with parallel tasks enabled.
         this.mustRunAfter(this.getProject().getTasks().getByName("build"));
@@ -154,11 +142,11 @@ public class TaskDiluvUpload extends DefaultTask {
     
     public void addGameVersion (String version) {
         
-        LOGGER.debug("Adding game version {} to project {}.", version, this.projectId);
+        this.log.debug("Adding game version {} to project {}.", version, this.projectId);
         
         if (!this.gameVersions.add(version)) {
             
-            LOGGER.warn("The game version {} was already applied for project {}.", version, this.projectId);
+            this.log.warn("The game version {} was already applied for project {}.", version, this.projectId);
         }
     }
     
@@ -180,7 +168,7 @@ public class TaskDiluvUpload extends DefaultTask {
      */
     public void addDependency (long project) {
         
-        this.addRelation(project, RELATION_REQUIRED);
+        this.addRelation(project, Constants.RELATION_REQUIRED);
     }
     
     /**
@@ -190,7 +178,7 @@ public class TaskDiluvUpload extends DefaultTask {
      */
     public void addOptionalDependency (long project) {
         
-        this.addRelation(project, RELATION_OPTIONAL);
+        this.addRelation(project, Constants.RELATION_OPTIONAL);
     }
     
     /**
@@ -200,7 +188,7 @@ public class TaskDiluvUpload extends DefaultTask {
      */
     public void addIncompatibility (long project) {
         
-        this.addRelation(project, RELATION_INCOMPATIBLE);
+        this.addRelation(project, Constants.RELATION_INCOMPATIBLE);
     }
     
     /**
@@ -212,27 +200,27 @@ public class TaskDiluvUpload extends DefaultTask {
      */
     public void addRelation (long project, String type) {
         
-        if (!PROJECT_RELATION_TYPES.contains(type)) {
+        if (!Constants.PROJECT_RELATION_TYPES.contains(type)) {
             
-            LOGGER.warn("The project relation type {} is not recognized and may not work. The known types are {}.", type, PROJECT_RELATION_TYPES.stream().collect(Collectors.joining(", ")));
+            this.log.warn("The project relation type {} is not recognized and may not work. The known types are {}.", type, Constants.PROJECT_RELATION_TYPES.stream().collect(Collectors.joining(", ")));
         }
         
         final String existingRelation = this.projectRelations.put(project, type);
-        LOGGER.debug("Added {} relation with project {}.", type, project);
+        this.log.debug("Added {} relation with project {}.", type, project);
         
         if (existingRelation != null) {
             
-            LOGGER.warn("Overwriting relation for {} to {}, was previously set as {}.", project, type, existingRelation);
+            this.log.warn("Overwriting relation for {} to {}, was previously set as {}.", project, type, existingRelation);
         }
     }
     
     public void addLoader (String loader) {
         
-        LOGGER.debug("Adding loader tag {} to project {}.", loader, this.projectId);
+        this.log.debug("Adding loader tag {} to project {}.", loader, this.projectId);
         
         if (!this.loaders.add(loader)) {
             
-            LOGGER.warn("The loader tag {} was already applied for project {}.", loader, this.projectId);
+            this.log.warn("The loader tag {} was already applied for project {}.", loader, this.projectId);
         }
     }
     
@@ -244,7 +232,7 @@ public class TaskDiluvUpload extends DefaultTask {
             // Attempt to automatically resolve the game version if one wasn't specified.
             if (this.gameVersions.isEmpty()) {
                 
-                final String detectedVersion = detectGameVersion(this.getProject());
+                final String detectedVersion = this.detectGameVersion();
                 
                 if (detectedVersion == null) {
                     
@@ -263,9 +251,9 @@ public class TaskDiluvUpload extends DefaultTask {
             }
             
             // Only semantic versioning is allowed.
-            if (!SEM_VER.matcher(this.projectVersion).matches()) {
+            if (!Constants.SEM_VER.matcher(this.projectVersion).matches()) {
                 
-                LOGGER.error("Project version {} is not semantic versioning compatible. The file can not be uploaded. https://semver.org", this.projectVersion);
+                this.log.error("Project version {} is not semantic versioning compatible. The file can not be uploaded. https://semver.org", this.projectVersion);
                 throw new GradleException("Project version '" + this.projectVersion + "' is not semantic versioning compatible. The file can not be uploaded. https://semver.org");
             }
             
@@ -280,7 +268,7 @@ public class TaskDiluvUpload extends DefaultTask {
             // Ensure the file actually exists before trying to upload it.
             if (file == null || !file.exists()) {
                 
-                LOGGER.error("The upload file is missing or null. {}", this.uploadFile);
+                this.log.error("The upload file is missing or null. {}", this.uploadFile);
                 throw new GradleException("The upload file is missing or null. " + String.valueOf(this.uploadFile));
             }
             
@@ -295,14 +283,14 @@ public class TaskDiluvUpload extends DefaultTask {
                 
                 catch (final IOException e) {
                     
-                    LOGGER.error("Failed to upload the file!", e);
+                    this.log.error("Failed to upload the file!", e);
                     throw new GradleException("Failed to upload the file!", e);
                 }
             }
             
             catch (final URISyntaxException e) {
                 
-                LOGGER.error("Invalid endpoint URI!", e);
+                this.log.error("Invalid endpoint URI!", e);
                 throw new GradleException("Invalid endpoint URI!", e);
             }
         }
@@ -311,8 +299,8 @@ public class TaskDiluvUpload extends DefaultTask {
             
             if (this.failSilently) {
                 
-                LOGGER.info("Failed to upload to Diluv. Check logs for more info.");
-                LOGGER.error("Diluv upload failed silently.", e);
+                this.log.info("Failed to upload to Diluv. Check logs for more info.");
+                this.log.error("Diluv upload failed silently.", e);
             }
             
             else {
@@ -331,7 +319,7 @@ public class TaskDiluvUpload extends DefaultTask {
      */
     public void upload (URI endpoint, File file) throws IOException {
         
-        LOGGER.debug("Uploading {} to {}.", file.getPath(), this.getUploadEndpoint());
+        this.log.debug("Uploading {} to {}.", file.getPath(), this.getUploadEndpoint());
         
         final HttpClient client = HttpClientBuilder.create().setDefaultRequestConfig(RequestConfig.custom().setCookieSpec(CookieSpecs.IGNORE_COOKIES).build()).build();
         final HttpPost post = new HttpPost(endpoint);
@@ -351,7 +339,7 @@ public class TaskDiluvUpload extends DefaultTask {
         data.setLoaders(this.loaders);
         data.setDependencies(this.projectRelations.entrySet().stream().map(e -> new FileDependency(e.getKey(), e.getValue())).collect(Collectors.toList()));
         
-        form.addTextBody("data", GSON.toJson(data), ContentType.APPLICATION_JSON);
+        form.addTextBody("data", this.gson.toJson(data), ContentType.APPLICATION_JSON);
         post.setEntity(form.build());
         
         try {
@@ -360,26 +348,26 @@ public class TaskDiluvUpload extends DefaultTask {
             final int status = response.getStatusLine().getStatusCode();
             final String responseBody = EntityUtils.toString(response.getEntity());
             
-            LOGGER.debug("Diluv Response Code: {}", status);
-            LOGGER.debug("Diluv Response Body: {}", responseBody);
+            this.log.debug("Diluv Response Code: {}", status);
+            this.log.debug("Diluv Response Body: {}", responseBody);
             
             if (status == 200) {
                 
-                this.uploadInfo = GSON.fromJson(responseBody, ResponseUpload.class);
-                LOGGER.info("Sucessfully uploaded {} to {} as file id {}.", file.getName(), this.projectId, this.uploadInfo.getId());
+                this.uploadInfo = this.gson.fromJson(responseBody, ResponseUpload.class);
+                this.log.info("Sucessfully uploaded {} to {} as file id {}.", file.getName(), this.projectId, this.uploadInfo.getId());
             }
             
             else {
                 
-                this.errorInfo = GSON.fromJson(responseBody, ResponseError.class);
-                LOGGER.error("Upload failed! Status: {} Reson: {}", status, this.errorInfo.getMessage());
+                this.errorInfo = this.gson.fromJson(responseBody, ResponseError.class);
+                this.log.error("Upload failed! Status: {} Reson: {}", status, this.errorInfo.getMessage());
                 throw new GradleException("Upload failed! Status: " + status + " Reson: " + this.errorInfo.getMessage());
             }
         }
         
         catch (final IOException e) {
             
-            LOGGER.error("Failure to upload file!", e);
+            this.log.error("Failure to upload file!", e);
             throw e;
         }
     }
@@ -439,8 +427,9 @@ public class TaskDiluvUpload extends DefaultTask {
      * @return A detected game version string. This will be null if nothing was found.
      */
     @Nullable
-    private static String detectGameVersion (Project project) {
+    public final String detectGameVersion () {
         
+        final Project project = this.getProject();
         String version = null;
         
         // ForgeGradle will store the game version here.
@@ -471,11 +460,11 @@ public class TaskDiluvUpload extends DefaultTask {
             
             catch (final Exception e) {
                 
-                project.getLogger().debug("Failed to detect loom game version.", e);
+                this.log.debug("Failed to detect loom game version.", e);
             }
         }
         
-        project.getLogger().debug("Using fallback game version {}.", version);
+        this.log.debug("Using fallback game version {}.", version);
         return version;
     }
 }
